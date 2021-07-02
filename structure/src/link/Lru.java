b6ee5312f19520a73b9515728ce4773e7a8d9718
jiyunchen <627385509@qq.com>
2021-07-02 23:26:21 +0800

    hello

--- structure/src/link/Lru.java
+++ structure/src/link/Lru.java
@@ -2,6 +2,23 @@ package link;
 
 public class Lru {
 
+    /**
+     * Lru:Least Recently used
+     * Get: get value from cache and update index positon
+     * Put: when the cache is not full,add this new element to the end,
+     *      if cache is full,firstly delete the last element and
+     *      add this new one to the end
+     * @param args
+     */
+
+    public int getValue(String key){
+        return 0;
+    }
+
+    public void putValue(String key,int value){
+
+    }
+
     public static void main(String[] args) {
         System.out.println("hello world");
     }
